54a093eda1baa057ee71a5507af4d9c99af93df6
kylestech95 <kylestech95@penguin>
2024-09-21 11:50:21 -0400

    feat: create database query

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bp = require('body-parser')
 const PORT = 9939
 const path = require('path')
+const pool = require('./db.js').pool
 
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
@@ -12,9 +13,11 @@ app.use(express.static(path.resolve(__dirname,'../client')))
 
         
 // route
-app.route('/api/db/write').post((req,res)=>{
+app.route('/api/db/write').post(async(req,res)=>{
     const {type,name,schema} = req.body
     console.log(req.body)
+    let createDB = await pool.query(`create database ${name};`)
+    console.log(createDB)
     res.send('db written')
 })
 
--- server/db.js
+++ server/db.js
@@ -1,1 +1,25 @@
-const pg = require('pg')
+const Pool = require("pg-pool");
+require("dotenv").config();
+// const Sequelize = require("sequelize").Sequelize;
+
+// let sequelize;
+// if (process.env.URI) {
+//   sequelize = new Sequelize(process.env.URI);
+// } else {
+//   sequelize = new Sequelize(process.env.DB, process.env.DBU, process.env.DBPD,  {
+//     host: process.env.DBH,
+//     dialect: "postgres",
+//     ssl: true,
+//   });
+// }
+
+// psql db
+const pool = new Pool({
+  database: process.env.DB,
+  user: process.env.DBU,
+  host: process.env.DBH,
+  port: process.env.DBP,
+  password: process.env.DBPD,
+});
+
+module.exports = { pool };
